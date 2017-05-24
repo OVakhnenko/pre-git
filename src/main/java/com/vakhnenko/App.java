7ac61edc468cc64fd65df018d136c37e9ff66475
Aleksey <oleksiy@vakhnenko.com>
2017-05-24 20:40:07 +0300

    ...

--- src/main/java/com/vakhnenko/App.java
+++ src/main/java/com/vakhnenko/App.java
@@ -5,3 +5,21 @@ public class App {
         System.out.println("Hello World!");
     }
 }
+
+/*
+
+public void processFilesFromFolder(File folder)
+{
+    File[] folderEntries = folder.listFiles();
+    for (File entry : folderEntries)
+    {
+        if (entry.isDirectory())
+        {
+            processFilesFromFolder(entry);
+            continue;
+        }
+        // иначе вам попался файл, обрабатывайте его!
+    }
+}
+
+ */
